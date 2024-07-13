7c4c3a9a18e857d7e0a194abc54c09da00684270
BillChan226 <billchenzr226@outlook.com>
2024-07-14 00:16:31 +0800

    add data

--- static/js/benmark_table.js
+++ static/js/benmark_table.js
@@ -103,12 +103,12 @@ var barColorFn = function (value, formatterParams) {
 
 document.addEventListener('DOMContentLoaded', function () {
     Promise.all([
-        fetch('static/data/benchmark.json').then(response => response.json()),
-        fetch('static/data/human_eval.json').then(response => response.json()),
-        fetch('static/data/alignment.json').then(response => response.json()),
-        fetch('static/data/safety.json').then(response => response.json()),
-        fetch('static/data/quality.json').then(response => response.json()),
-        fetch('static/data/bias.json').then(response => response.json()),
+        fetch('data/agentpoison/benchmark.json').then(response => response.json()),
+        fetch('data/agentpoison/human_eval.json').then(response => response.json()),
+        fetch('data/agentpoison/alignment.json').then(response => response.json()),
+        fetch('data/agentpoison/safety.json').then(response => response.json()),
+        fetch('data/agentpoison/quality.json').then(response => response.json()),
+        fetch('data/agentpoison/bias.json').then(response => response.json()),
     ])
         .then(([
             benchmark_tabledata,
